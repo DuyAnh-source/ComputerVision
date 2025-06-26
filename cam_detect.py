de4d69be47f3275506ea785b92d5fc38bedc1fe7
duyAnh <duyanh2002hp@gmail.com>
2025-06-26 16:02:48 +0700

    Add distance #2

--- cam_detect.py
+++ cam_detect.py
@@ -1,5 +1,10 @@
 import cv2
-import os
+import math
+
+W_real = 15.0  # cm
+h_real = 20.0  # cm
+focal_length = 600  # lấy từ bước hiệu chuẩn
+real_diag = math.sqrt(W_real ** 2 + h_real ** 2)
 
 # Load mô hình CNN
 modelFile = "res10_300x300_ssd_iter_140000.caffemodel"
@@ -28,12 +33,19 @@ while True:
     # Duyệt qua các detection
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
-        if confidence > 0.5:
+        if confidence > 0.9:
             box = detections[0, 0, i, 3:7] * [w, h, w, h]
             (startX, startY, endX, endY) = box.astype("int")
+            width = endX - startX
+            height = endY - startY
+            centreX = (startX + width // 2) // w
+            centreY = (startY + height // 2) // h
+            perceived_diag = math.sqrt(width ** 2 + height ** 2)
+
+            distance = (real_diag * focal_length) / perceived_diag
 
             # Vẽ bounding box và hiển thị độ tin cậy
-            text = f"{confidence:.2f}"
+            text = f"{confidence:.2f} {distance:.2f}"
             cv2.rectangle(frame, (startX, startY), (endX, endY),
                           (0, 255, 0), 2)
             cv2.putText(frame, text, (startX, startY - 10),
